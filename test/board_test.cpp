4c3053f8240a9c9ace919f6d4ac40a966f3c4ae2
Tyler Freitas <tjfr8s@gmail.com>
2017-10-17 10:51:32 -0700

    check number of neighbors for a given cell using Board.getNeighbor(int, int) member function

--- test/board_test.cpp
+++ test/board_test.cpp
@@ -3,9 +3,19 @@
 #include "../cell.h"
 #include "../board.h"
 
+// void playGame();
+
 int main()
 {
     Board myBoard;
     myBoard.printBoard();
+    myBoard.m_board[0][14].toggleStatus();
+    myBoard.printBoard();
+    std::cout << myBoard.numNeighbors(13, 0) << std::endl;
     return 0;
 }
+
+// void playGame()
+// {
+//     Board myBoard
+// }
